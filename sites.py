065c355de503168cfeef0f7ecefd181cd8cd48ed
tps12 <tps12@columbia.edu>
2010-09-27 19:38:44 -0400

    Traits in [0,1].

--- sites.py
+++ sites.py
@@ -21,7 +21,7 @@ class Simulation:
                                    (self.dimensions[0] + 1) * self.grid_size,
                                    (self.dimensions[1] + 1) * self.grid_size)
 
-        self.sites = [[[random.randint(1,10) for i in range(5)]
+        self.sites = [[[random.random() for i in range(5)]
                        for y in range(self.dimensions[1])]
                       for x in range(self.dimensions[0])]
 
@@ -29,7 +29,7 @@ class Simulation:
         screen.fill((255,255,255), self.bg_rect)
 
     def similarity(self, p1, p2):
-        return sum((c1 - c2)*(c1-c2)/100. for c1, c2 in zip(p1, p2))/float(len(p1))
+        return sum((c1 - c2)*(c1-c2) for c1, c2 in zip(p1, p2))/float(len(p1))
 
     def color(self, p1, p2):
         gray = 255 - int(self.similarity(p1, p2) * 255)
